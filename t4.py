1b9115ef3776f6bc7835469a2f28095fd3e4ff40
Guruprasad Patil <patilguruprasad2004@gmail.com>
2026-09-23 07:31:45 +0530

    unit testing

---
+++ t4.py
@@ -0,0 +1,11 @@
+from c2 import square
+
+def main():
+    test_square()
+
+def test_square():
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(0) == 0
