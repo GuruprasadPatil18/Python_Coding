bccdf78c8424cb6ed81878fec801d323743bed21
GuruprasadPatil18 <119645975+GuruprasadPatil18@users.noreply.github.com>
2026-09-25 06:39:30 +0530

    Implement name collection and farewell message

---
+++ adieu/adieu.py
@@ -0,0 +1,22 @@
+names = []
+
+while True:
+    try:
+        print("Name: ",end="")
+        name = input()
+        names.append(name)
+        print(names)
+    except EOFError:
+        print()
+        break
+
+if len(names) == 1:
+    result = names[0]
+
+elif len(names) == 2:
+    result = " and ".join(names)
+
+else:
+    result = ", ".join(names[:-1]) + ", and " + names[-1]
+
+print("Adieu, adieu, to " + result)
