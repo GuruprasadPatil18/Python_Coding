e89331ef45cc91232a77343ca67329aaf396446f
GuruprasadPatil18 <119645975+GuruprasadPatil18@users.noreply.github.com>
2026-09-25 06:48:00 +0530

    Add fuel.py for fraction input and percentage calculation
    Implement a loop to input and validate fractions, calculate percentage, and print status.

---
+++ fuel/fuel.py
@@ -0,0 +1,26 @@
+while True:
+    try:
+        x, y = input("Fraction: ").split("/")
+
+        x = int(x)
+        y = int(y)
+
+        if x < 0 or x > y:
+            continue
+
+        percentage = round((x / y) * 100)
+
+        if percentage <= 1:
+            print("E")
+        elif percentage >= 99:
+            print("F")
+        else:
+            print(f"{percentage}%")
+
+        break
+
+    except ValueError:
+        pass
+
+    except ZeroDivisionError:
+        pass
